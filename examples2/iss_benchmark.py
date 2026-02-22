b1828f287fbce6bbb8ab12ed379013d7e3a036be
lg <leo@leog.dev>
2026-02-22 18:30:06 +0100

    benchmark

--- examples2/iss_benchmark.py
+++ examples2/iss_benchmark.py
@@ -8,28 +8,88 @@
 """
 
 from datetime import timedelta
-from time import perf_counter
+from time import perf_counter, sleep
+from typing import Any
 
-from leorbit2.m import Quantity
-from leorbit2.time import Time, TimeInterval
-from leorbit2 import get_satellite
+import numpy as np
+from pydantic import BaseModel
+import requests
 
+from leorbit.coordinates import GPS
+from leorbit.frames import AbsoluteFrame
+from leorbit.m import Quantity
+from leorbit.time import Time, TimeInterval
+from leorbit import get_satellite
+from leorbit.m import Vector3
+
+class OpenNotifyPosition(BaseModel):
+    latitude: str # [degrees]
+    longitude: str # [degrees]
+
+class OpenNotifyIssResponse(BaseModel):
+    """http://api.open-notify.org/iss-now.json"""
+    message: str
+    timestamp: int
+    iss_position: OpenNotifyPosition
+
+    @staticmethod
+    def retrieve() -> OpenNotifyIssResponse:
+        response = requests.get("http://api.open-notify.org/iss-now.json")
+        return OpenNotifyIssResponse(**response.json())
+    
+    @property
+    def epoch(self) -> Time:
+        return Time(self.timestamp)
+
+    @property
+    def gps(self) -> GPS:
+        return GPS(
+            latitude=float(self.iss_position.latitude) * Quantity.deg,
+            longitude=float(self.iss_position.longitude) * Quantity.deg,
+            altitude=400 * Quantity.kilo_meter
+        )
+    
+def _repr_vec_pos(v: Vector3[Any]) -> str:
+    vals = v._values.flatten()
+    return f"Position: ({vals[0]:.0f} m, {vals[1]:.0f} m, {vals[2]:.0f} m)"
 
 def main():
     # build a satellite object once
     iss = get_satellite(25544, log=True)
 
-    # pick a fixed epoch to propagate from
-    t0 = Time.now()
+    # pick a fixed epoch to propagate from and include margin for API timestamp drift
+    t0 = Time.now() - timedelta(hours=2)
     timeline = TimeInterval(
         t0,
-        t0 + timedelta(hours=1),
+        t0 + timedelta(hours=4),
     )
 
     _time_flag = perf_counter()
     trajectory = iss.trajectory(timeline)
-    print(f"Computed trajectory for 1 hour with 1s step in {perf_counter() - _time_flag:.4f}s")
-    print(trajectory.name)
+    print(f"Computed trajectory for 4 hours with 1s step in {perf_counter() - _time_flag:.4f}s")
+
+    for _ in range(16):
+        open_notify_coords = OpenNotifyIssResponse.retrieve()
+        open_notify_gps = open_notify_coords.gps
+        epoch = open_notify_coords.epoch
+        gps = trajectory.gps_at(epoch)
+
+        on_pos_itrf = open_notify_coords.gps.to_coordinates(epoch).get_pos(AbsoluteFrame.ITRF)
+        leorbit_pos_itrf = trajectory.get_pos(epoch, AbsoluteFrame.ITRF)
+
+        # display results
+        print(f"At epoch {epoch.isoformat}: ")
+        print(f"Open notify position: {open_notify_gps.dms}")
+        print(f"Leorbit position:     {gps.dms}")
+
+        # compute simple latitude/longitude differences in degrees
+        dlat = float(np.asarray((gps.latitude - open_notify_gps.latitude).magnitude("deg")).reshape(-1)[0])
+        dlon = float(np.asarray((gps.longitude - open_notify_gps.longitude).magnitude("deg")).reshape(-1)[0])
+        dlat = abs(dlat)
+        dlon = abs(dlon)
+        print(f"Difference: Δlat={dlat:.4f}°, Δlon={dlon:.4f}°")
+
+        sleep(5)
 
 if __name__ == "__main__":
     main()
